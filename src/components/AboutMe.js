2dd39031a84379046e6e32cfe5f2b7a94a11477f
saurav2021c <saurav2021c@gmail.com>
2024-09-01 10:25:37 +0530

    4th commit

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -12,24 +12,14 @@ const AboutMe = () => {
         <div className="col-lg-6 col-xm-12">
           <h1 className="about-heading">About Me</h1>
           <p className="aboutme-para">
-            As a Master's in Data Science student with a strong foundation in
-            data analytics, business development, and web design, I am eager to
-            apply my skills in short-term campus assignments. My recent Data
-            Analytics Internship at NA Sportz Interactive enhanced my abilities
-            in data analysis, metric development, and cross-functional
-            collaboration, while my experience as a Business Development
-            Specialist at Standard Multimodal Logistics Pvt Ltd honed my client
-            relations, market research, and strategic partnership skills.
-            Proficient in Python, MS Office, and Power BI, I am confident in my
-            ability to contribute effectively across departments. My
-            adaptability, demonstrated through various roles in web design, data
-            analysis, and volunteer work, ensures I can quickly adjust to new
-            environments and tackle diverse challenges. I am excited about the
-            opportunity to work with Student Temporary Services to gain
-            professional experience, network with campus hiring managers, and
-            contribute meaningfully to the university community, believing that
-            my background, skills, and enthusiasm for learning will make me a
-            valuable asset to your team.
+            As a Master's in Data Science student at DePaul University, I bring
+            a unique blend of technical expertise, leadership, and
+            organizational skills to every project I undertake. With experience
+            in data analysis, web design, and community outreach, I am committed
+            to driving impactful results through innovative solutions. My
+            passion for learning, coupled with a strong work ethic, fuels my
+            desire to contribute effectively to any team and make a meaningful
+            difference.
           </p>
         </div>
       </div>
